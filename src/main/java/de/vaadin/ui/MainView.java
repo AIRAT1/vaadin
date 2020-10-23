3993ed52125319c97429ae03508e3863b786afaf
Ayrat <ayrat1@mail.ru>
2020-10-23 18:36:07 +0300

    step10 complete. Create custom UI form.

--- src/main/java/de/vaadin/ui/MainView.java
+++ src/main/java/de/vaadin/ui/MainView.java
@@ -1,6 +1,8 @@
 package de.vaadin.ui;
 
+import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.value.ValueChangeMode;
@@ -10,18 +12,28 @@ import de.vaadin.backend.entity.Contact;
 import de.vaadin.backend.service.ContactService;
 
 @Route("")
+@CssImport("./styles/shared-styles.css")
 public class MainView extends VerticalLayout {
     Grid<Contact> grid = new Grid<>(Contact.class);
     TextField filterText = new TextField();
     private ContactService contactService;
+    private final ContactForm form;
 
     public MainView(ContactService contactService) {
         this.contactService = contactService;
         addClassName("list-view");
         setSizeFull();
+
         configureGrid();
         configureFilter();
-        add(filterText, grid);
+
+        form = new ContactForm();
+
+        Div content = new Div(grid, form);
+        content.addClassName("content");
+        content.setSizeFull();
+
+        add(filterText, content);
         updateList();
     }
 
