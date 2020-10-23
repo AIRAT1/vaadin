fc1f7b6628b7e7cdd144a2c204a3626081073321
Ayrat <ayrat1@mail.ru>
2020-10-23 16:17:48 +0300

    step8 complete. Listing data in grid and configure columns.

--- src/main/java/de/vaadin/ui/MainView.java
+++ src/main/java/de/vaadin/ui/MainView.java
@@ -1,18 +1,40 @@
 package de.vaadin.ui;
 
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.datepicker.DatePicker;
-import com.vaadin.flow.component.html.Paragraph;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
+import de.vaadin.backend.entity.Company;
+import de.vaadin.backend.entity.Contact;
+import de.vaadin.backend.service.ContactService;
 
 @Route("")
 public class MainView extends VerticalLayout {
+    Grid<Contact> grid = new Grid<>(Contact.class);
+    private ContactService contactService;
 
-    public MainView() {
-        Button button = new Button("Qwerty");
-        add(button);
+    public MainView(ContactService contactService) {
+        this.contactService = contactService;
+        addClassName("list-view");
+        setSizeFull();
+        configureGrid();
+        add(grid);
+        updateList();
+    }
+
+    private void configureGrid() {
+        grid.addClassName("contact-grid");
+        grid.setSizeFull();
+        grid.removeColumnByKey("company");
+        grid.setColumns("firstName", "lastName", "email", "status");
+        grid.addColumn(contact -> {
+            Company company = contact.getCompany();
+            return company == null ? "-" : company.getName();
+        }).setHeader("Company").setSortable(true);
+        grid.getColumns().forEach(col -> col.setAutoWidth(true));
+    }
+
+    private void updateList() {
+        grid.setItems(contactService.findAll());
     }
 
 }
