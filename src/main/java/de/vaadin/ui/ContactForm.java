2051f30a31ba72526e5c154bacdf9de61524a908
Ayrat <ayrat1@mail.ru>
2020-10-23 20:23:53 +0300

    part11 complete add data binding

--- src/main/java/de/vaadin/ui/ContactForm.java
+++ src/main/java/de/vaadin/ui/ContactForm.java
@@ -1,5 +1,8 @@
 package de.vaadin.ui;
 
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.ComponentEvent;
+import com.vaadin.flow.component.ComponentEventListener;
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
@@ -8,10 +11,17 @@ import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.textfield.EmailField;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.BeanValidationBinder;
+import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.data.binder.ValidationException;
+import com.vaadin.flow.shared.Registration;
 import de.vaadin.backend.entity.Company;
 import de.vaadin.backend.entity.Contact;
 
+import java.util.List;
+
 public class ContactForm extends FormLayout {
+
     TextField firstName = new TextField("First name");
     TextField lastName = new TextField("Last name");
     EmailField email = new EmailField("Email");
@@ -22,17 +32,33 @@ public class ContactForm extends FormLayout {
     Button delete = new Button("Delete");
     Button close = new Button("Cancel");
 
-    public ContactForm() {
+    Binder<Contact> binder = new BeanValidationBinder<>(Contact.class);
+    private Contact contact;
+
+    public ContactForm(List<Company> companies) {
         addClassName("contact-form");
-        add(firstName,
+
+        binder.bindInstanceFields(this);
+        status.setItems(Contact.Status.values());
+        company.setItems(companies);
+        company.setItemLabelGenerator(Company::getName);
+
+        add(
+                firstName,
                 lastName,
                 email,
-                company,
                 status,
-                createButtonsLayout());
+                company,
+                createButtonsLayout()
+        );
     }
 
-    private HorizontalLayout createButtonsLayout() {
+    public void setContact(Contact contact) {
+        this.contact = contact;
+        binder.readBean(contact);
+    }
+
+    private Component createButtonsLayout() {
         save.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
         delete.addThemeVariants(ButtonVariant.LUMO_ERROR);
         close.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
@@ -40,6 +66,60 @@ public class ContactForm extends FormLayout {
         save.addClickShortcut(Key.ENTER);
         close.addClickShortcut(Key.ESCAPE);
 
+        save.addClickListener(click -> validateAndSave());
+        delete.addClickListener(click -> fireEvent(new DeleteEvent(this, contact)));
+        close.addClickListener(click -> fireEvent(new CloseEvent(this)));
+
+        binder.addStatusChangeListener(evt -> save.setEnabled(binder.isValid()));
+
         return new HorizontalLayout(save, delete, close);
+    }
+
+    private void validateAndSave() {
+
+        try {
+            binder.writeBean(contact);
+            fireEvent(new SaveEvent(this, contact));
+        } catch (ValidationException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Events
+    public static abstract class ContactFormEvent extends ComponentEvent<ContactForm> {
+        private Contact contact;
+
+        protected ContactFormEvent(ContactForm source, Contact contact) {
+            super(source, false);
+            this.contact = contact;
+        }
+
+        public Contact getContact() {
+            return contact;
+        }
+    }
+
+    public static class SaveEvent extends ContactFormEvent {
+        SaveEvent(ContactForm source, Contact contact) {
+            super(source, contact);
+        }
+    }
+
+    public static class DeleteEvent extends ContactFormEvent {
+        DeleteEvent(ContactForm source, Contact contact) {
+            super(source, contact);
+        }
+
+    }
+
+    public static class CloseEvent extends ContactFormEvent {
+        CloseEvent(ContactForm source) {
+            super(source, null);
+        }
+    }
+
+    public <T extends ComponentEvent<?>> Registration addListener(Class<T> eventType,
+                                                                  ComponentEventListener<T> listener) {
+        return getEventBus().addListener(eventType, listener);
     }
 }
--- src/main/java/de/vaadin/ui/MainView.java
+++ src/main/java/de/vaadin/ui/MainView.java
@@ -1,43 +1,42 @@
 package de.vaadin.ui;
 
-import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.Route;
 import de.vaadin.backend.entity.Company;
 import de.vaadin.backend.entity.Contact;
+import de.vaadin.backend.service.CompanyService;
 import de.vaadin.backend.service.ContactService;
 
 @Route("")
 @CssImport("./styles/shared-styles.css")
 public class MainView extends VerticalLayout {
+
+    private final ContactForm form;
+    Grid<Contact> grid = new Grid<>(Contact.class);
+    TextField filterText = new TextField();
+
     private ContactService contactService;
 
-    private Grid<Contact> grid = new Grid<>(Contact.class);
-    private TextField filterText = new TextField();
-    private Button addContactButton = new Button("Add contact");
-    private ContactForm form;
-
-    public MainView(ContactService contactService) {
+    public MainView(ContactService contactService,
+                    CompanyService companyService) {
         this.contactService = contactService;
         addClassName("list-view");
         setSizeFull();
-
         configureGrid();
         configureFilter();
 
-        form = new ContactForm();
+        form = new ContactForm(companyService.findAll());
 
         Div content = new Div(grid, form);
         content.addClassName("content");
         content.setSizeFull();
 
-        add(new HorizontalLayout(filterText, addContactButton), content);
+        add(filterText, content);
         updateList();
     }
 
@@ -49,7 +48,8 @@ public class MainView extends VerticalLayout {
         grid.addColumn(contact -> {
             Company company = contact.getCompany();
             return company == null ? "-" : company.getName();
-        }).setHeader("Company").setSortable(true);
+        }).setHeader("Company");
+
         grid.getColumns().forEach(col -> col.setAutoWidth(true));
     }
 
